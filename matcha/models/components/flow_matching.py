1a2d66d8df5b2f8bc13c3084c1879b74b31dcd67
Mocifiel <867081920@qq.com>
2024-05-15 12:50:20 +0800

    nfdm class modified

--- matcha/models/components/flow_matching.py
+++ matcha/models/components/flow_matching.py
@@ -2,9 +2,13 @@ from abc import ABC
 
 import torch
 import torch.nn.functional as F
+import torch.nn as nn
+from torch.func import jvp
+import numpy as np
 
 from matcha.models.components.decoder import Decoder
 from matcha.utils.pylogger import get_pylogger
+from einops import rearrange
 
 log = get_pylogger(__name__)
 
@@ -154,8 +158,17 @@ class NFDM(BASECFM):
 
         in_channels = in_channels + (spk_emb_dim if n_spks > 1 else 0)
         # Just change the architecture of the estimator here
-        self.predictor = Decoder(in_channels=in_channels, out_channels=out_channel, **decoder_params) # x_theta_
-        self.estimator = Decoder(in_channels=in_channels, out_channels=2*out_channel, **decoder_params)
+        self.x_predictor = Decoder(in_channels=in_channels, out_channels=out_channel, **decoder_params) # x_theta_
+        self.mean_predictor = Decoder(in_channels=in_channels, out_channels=out_channel, **decoder_params)
+        self.var_predictor = nn.Sequential(Decoder(in_channels=in_channels, out_channels=out_channel, **decoder_params),
+                                           nn.Softplus())
+        self.g_phi = nn.Sequential(nn.Linear(1,16),
+                                   nn.ReLU(True),
+                                   nn.Linear(16,8),
+                                   nn.ReLU(True),
+                                   nn.Linear(8,1),
+                                   nn.Softplus())
+        self.delta = 1e-2
     
     
     def forward(self, mu, mask, n_timesteps, temperature=1.0, spks=None, cond=None, uncond_spks=None,cfk=0):
@@ -183,3 +196,182 @@ class NFDM(BASECFM):
         z = torch.randn_like(mu) * temperature
         t_span = torch.linspace(0, 1, n_timesteps + 1, device=mu.device)
         return self.solve_euler(z, t_span=t_span, mu=mu, mask=mask, spks=spks, cond=cond, uncond_spks=uncond_spks, cfk=cfk)
+    
+    def forward_phi(self, eps, t, x, mu, mask):
+        """
+        The marginal distribution q_phi(zt|x) of the forward process
+        where x is the data point, zt is the intermediate latent variable
+        zt is reparameteriazed by eps, a standard gaussian variable ~ N(0, I)
+        
+        Args:
+            eps (torch.Tensor):  should be a standard gaussian variable ~ N(0, I)
+                shape: (batch_size, n_feats, mel_timesteps)
+            t (torch.Tensor): timesteps
+                shape (batch_size)
+            x (torch.Tensor): the original data point
+                shape (batch_size, n_feats, mel_timesteps)
+            mu (torch.Tensor): output of encoder
+                shape: (batch_size, n_feats, mel_timesteps)
+            mask (torch.Tensor): output_mask
+                shape: (batch_size, 1, mel_timesteps)
+        
+        Returns:
+            z (torch.Tensor): the intermediate latent variable
+                shape (batch_size, n_feats, mel_timesteps)
+        """
+        t_expand = rearrange(t, 'b -> b 1 1')
+        mean = t_expand*x + t_expand*(1-t_expand)*self.mean_predictor(x, mask, mu, t)
+        var = self.delta + ((1-t_expand)*
+                            self.var_predictor(t_expand*x, mask, mu, (1-t))/
+                            self.var_predictor(torch.zeros_like(x),mask,mu,torch.ones_like(t)))
+        z = mean + eps*var
+        return z
+    
+    def forward_sde(self, z, t, x, mu, mask):
+        """
+        The forward sde
+        
+        Args:
+            z (torch.Tensor):  the intermediate latent variable
+                shape: (batch_size, n_feats, mel_timesteps)
+            t (torch.Tensor): timesteps
+                shape (batch_size)
+            x (torch.Tensor): the original data point
+                shape (batch_size, n_feats, mel_timesteps)
+            mu (torch.Tensor): output of encoder
+                shape: (batch_size, n_feats, mel_timesteps)
+            mask (torch.Tensor): output_mask
+                shape: (batch_size, 1, mel_timesteps)
+        
+        Returns:
+            dzdt (torch.Tensor): the intermediate latent variable
+                shape (batch_size, n_feats, mel_timesteps)
+        """
+        t_expand = rearrange(t, 'b -> b 1 1')
+        mean = t_expand*x + t_expand*(1-t_expand)*self.mean_predictor(x, mask, mu, t)
+        var = self.delta + ((1-t_expand)*
+                            self.var_predictor(t_expand*x, mask, mu, (1-t))/
+                            self.var_predictor(torch.zeros_like(x),mask,mu,torch.ones_like(t)))
+        eps = (z-mean)/var
+        _, dzdt = jvp(self.forward_phi,(eps, t, x, mu, mask),(torch.zeros_like(eps),
+                                                              torch.ones_like(t),
+                                                              torch.zeros_like(x),
+                                                              torch.zeros_like(mu),
+                                                              torch.zeros_like(mask)))
+        # too much function evaluation, can it be reduced? (later)
+
+
+        g_phi_t = self.g_phi(t_expand)
+
+        log_q_phi = self.log_q_phi(z,t,x,mu,mask)
+        score = torch.autograd.grad(outputs=log_q_phi,inputs=z,create_graph=True)
+        drift = dzdt + g_phi_t**2/2 * score
+    
+    def log_q_phi(self, z, t, x, mu, mask):
+        '''
+        log(q_phi(z_t | x)) = log(q(eps)) - log|J_F|
+
+        Args:
+            z (torch.Tensor):  the intermediate latent variable
+                shape: (batch_size, n_feats, mel_timesteps)
+            t (torch.Tensor): timesteps
+                shape (batch_size)
+            x (torch.Tensor): the original data point
+                shape (batch_size, n_feats, mel_timesteps)
+            mu (torch.Tensor): output of encoder
+                shape: (batch_size, n_feats, mel_timesteps)
+            mask (torch.Tensor): output_mask
+                shape: (batch_size, 1, mel_timesteps)
+        
+        Returns:
+            logp (torch.Tensor): the intermediate latent variable
+                shape (batch_size, )
+        '''
+
+        t_expand = rearrange(t, 'b -> b 1 1')
+        mean = t_expand*x + t_expand*(1-t_expand)*self.mean_predictor(x, mask, mu, t)
+        var = self.delta + ((1-t_expand)*
+                            self.var_predictor(t_expand*x, mask, mu, (1-t))/
+                            self.var_predictor(torch.zeros_like(x),mask,mu,torch.ones_like(t)))
+        eps = (z-mean)/var
+
+        D = np.prod(z.shape[1:])
+        
+        return -D/2. *np.log(2*np.pi) - torch.sum(eps**2,dim=(1,2))/2 -torch.sum(torch.log(var),dim=(1,2))
+    
+    def compute_loss(self, x1, mask, mu, spks=None, cond=None):
+        """Computes diffusion loss
+
+        Args:
+            x1 (torch.Tensor): Target
+                shape: (batch_size, n_feats, mel_timesteps)
+            mask (torch.Tensor): target mask
+                shape: (batch_size, 1, mel_timesteps)
+            mu (torch.Tensor): output of encoder
+                shape: (batch_size, n_feats, mel_timesteps)
+            spks (torch.Tensor, optional): speaker embedding. Defaults to None.
+                shape: (batch_size, spk_emb_dim)
+
+        Returns:
+            loss: conditional flow matching loss
+            y: conditional flow
+                shape: (batch_size, n_feats, mel_timesteps)
+        """
+        b, _, t = mu.shape
+
+        # random timestep
+        t = torch.rand([b, 1, 1], device=mu.device, dtype=mu.dtype)
+        # sample noise p(x_0)
+        z0 = torch.randn_like(x1)
+
+        # sample zt
+        mean = t*x1 + t*(1-t)*self.mean_predictor(x1, mask, mu, t.squeeze())
+        std = self.delta + ((1-t)*self.var_predictor(t*x1, mask, mu, (1-t.squeeze()))/self.var_predictor(torch.zeros_like(x1),mask,mu,torch.ones_like(t.squeeze())))
+        zt = mean + z0*std
+
+        # compute flow term and score term
+        _, flow = jvp(self.forward_phi,(z0, t.squeeze(), x1, mu, mask),(torch.zeros_like(z0),
+                                                              torch.ones_like(t.squeeze()),
+                                                              torch.zeros_like(x1),
+                                                              torch.zeros_like(mu),
+                                                              torch.zeros_like(mask)))
+        
+        g_phi_t = self.g_phi(t)
+        log_q_phi = self.log_q_phi(zt,t.squeeze(),x1,mu,mask)
+        
+        score = torch.autograd.grad(outputs=log_q_phi,inputs=zt,create_graph=True) # can be reduced to closed-form later.
+
+        # compute reverse SDE drift term, which is the training target
+        drift_target = flow - g_phi_t**2/2 *score
+
+        # compute the reverse SDE drift term incorporating the prediction of x:
+        x_pred = self.x_predictor(zt, mask, mu, t.squeeze(), spks, cond)
+        # compute flow term and score term
+        _, flow = jvp(self.forward_phi,(z0, t.squeeze(), x_pred, mu, mask),(torch.zeros_like(z0),
+                                                              torch.ones_like(t.squeeze()),
+                                                              torch.zeros_like(x_pred),
+                                                              torch.zeros_like(mu),
+                                                              torch.zeros_like(mask)))
+        
+        log_q_phi = self.log_q_phi(zt,t.squeeze(),x_pred,mu,mask)
+        score = torch.autograd.grad(outputs=log_q_phi,inputs=zt,create_graph=True)
+
+        drift = flow -g_phi_t**2/2 *score
+        loss = F.mse_loss(drift, drift_target, reduction="sum") / (torch.sum(mask) * drift_target.shape[1])
+        return loss, zt
+    
+    
+
+
+
+    
+
+
+
+
+
+
+
+
+
+        
